e4524c9fa36ed7828f38ec84712cac08d460dd9e
super_be <babarykin@list.ru>
2014-07-09 23:35:31 +0600

    Новый проект
    Гребаная гита опять переключила проект на себя. Руки оторвать
    программистам, кто написал такую гиту. Вопрос как вернуть TFS и как
    русифицировать гиту?

---
+++ Super_BE.Utilities.WWW/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Super_BE.Utilities.WWW.MvcApplication" Language="C#" %>
---
+++ Super_BE.Utilities.WWW/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Super_BE.Utilities.WWW.Startup))]
+namespace Super_BE.Utilities.WWW
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
